d65b3609442321db9d8a97ec2b338faf1bfca419
Evert van Brussel <ecvanbrussel@gmail.com>
2018-10-24 12:29:03 +0200

    Update stuff...

--- Sources/Double+Timepiece.swift
+++ Sources/Double+Timepiece.swift
@@ -22,22 +22,15 @@ public extension Double {
     }
     
     private func timeComponents(period: Period) -> DateComponents {
-        // First let's validate the value of self to be a valid time
-        let strSelf = "\(self)"
-        let range = NSMakeRange(0, strSelf.characters.count)
-        let pattern = "^(1?\\d|2[0-3])(\\.[0-5]\\d?)?$"
-        let regex = try! NSRegularExpression(pattern: pattern, options: [])
-        
-        guard regex.numberOfMatches(in: strSelf, options: [], range: range) > 0 else {
-            fatalError("Please provide a valid time.")
-        }
-        
         let formatter = DateFormatter()
         formatter.dateFormat = "h:mm a"
         let string = String(format: "%.2f \(period)", self)
-        let date = formatter.date(from: string)!
         
-        return DateComponents(hour: date.hour, minute: date.minute)
+        guard let date = formatter.date(from: string) else {
+            fatalError("Please provide a valid time.")
+        }
+        
+        return Calendar.current.dateComponents([.hour, .minute], from: date)
     }
 }
 
